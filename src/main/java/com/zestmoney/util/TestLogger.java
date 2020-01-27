ebf72c087d14dbcb9d13c82e8446b14105ebc30b
Saileshdash <52627780+Saileshdash@users.noreply.github.com>
2020-01-27 18:56:18 +0530

    updated testlogger

--- src/main/java/com/zestmoney/util/TestLogger.java
+++ src/main/java/com/zestmoney/util/TestLogger.java
@@ -17,51 +17,11 @@ public class TestLogger {
 		this.logger = Logger.getLogger(clazz);
 	}
 	
-	public void logDebug(final String message) {
-		logger.debug(message);
-		Reporter.log(message);
-	}
-
-	public void logError(final String message) {
-		logger.error(message);
-		Reporter.log(message);
-	}
-
-	public void logFatal(final String message) {
-		logger.fatal(message);
-		Reporter.log(message);
-	}
-
 	
 	public void logInfo(final String message) {
 		logger.info(message);
 		Reporter.log(message);
 	}
-
-	public void logSetupStep(final String message) {
-		logInfo("SetUp: " + message);
-	}
-
-	
-	public void logTearDownStep(final String message) {
-		logInfo("TearDown: " + message);
-	}
-	public void logTestStep(final String message) {
-		logInfo(message);
-	}
-
-	
-	public void logTestVerificationStep(final String message) {
-		logInfo("Verification: " + message);
-	}
-
-	
-	public void logWarn(final String message) {
-		logger.warn(message);
-		Reporter.log(message);
-	}
-
-	
 	
 	
 }
