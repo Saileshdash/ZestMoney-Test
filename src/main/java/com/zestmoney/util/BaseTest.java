d588cd8efc7047dafa0cb85736c119d6c32761c8
Saileshdash <52627780+Saileshdash@users.noreply.github.com>
2020-01-27 18:00:45 +0530

    commit

--- src/main/java/com/zestmoney/util/BaseTest.java
+++ src/main/java/com/zestmoney/util/BaseTest.java
@@ -7,17 +7,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 public class BaseTest {
-	
+
 	public WebDriver driver; 
-	
+
 	@BeforeClass
 	public void initBastTest() throws IOException{
 		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\driver\\chromeDriver\\windows\\chromedriver.exe");
 		driver = new ChromeDriver();
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
-		// driver.quit();
+		driver.quit();
 	}
 }
